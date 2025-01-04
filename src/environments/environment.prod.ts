a86be5331bf680824e6798aa4069874a39c50b8d
Xabi <Xabi@DESKTOP-OMEN>
2025-01-04 10:16:39 +0100

    CAMBIADO A LOAD BALANCER

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5e812b04b2694387af442e63969355e-911228447.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
